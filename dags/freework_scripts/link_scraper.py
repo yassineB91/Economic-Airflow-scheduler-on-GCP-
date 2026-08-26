12ce52ae6bbae871a52986b3ca72a1ad27a17f4c
Yassine <yassine@DESKTOP-KHAPAMM.localdomain>
2026-08-26 21:17:07 +0200

    correct Bigquery util class arguiment

--- dags/freework_scripts/link_scraper.py
+++ dags/freework_scripts/link_scraper.py
@@ -9,7 +9,6 @@ import sys
 
 logging.basicConfig(level=logging.info)
 logger =  logging.getLogger(__name__)
-client= bigquery.Client()
 
 def get_page_number(initial_url):
     """
@@ -71,7 +70,7 @@ def get_job_links(number_of_pages, initial_url):
             jobs.append(job_page)
 
             logger.info(f"Inserting {jobs} into table list_links")
-            bigquery_util.BigQuery(client).insert_rows(jobs,"dev-env-368414","freework","list_links")
+            bigquery_util.BigQuery().insert_rows(jobs,"dev-env-368414","freework","list_links")
 
 # def main():
 #     """
