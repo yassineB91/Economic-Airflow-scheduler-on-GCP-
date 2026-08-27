4d4661c666d115090d5ace200c401cde71b34921
Yassine <yassine@DESKTOP-KHAPAMM.localdomain>
2026-08-27 23:01:04 +0200

    add storing files in gcs before loading them into bigquery

--- dags/freework_scraping_dag.py
+++ dags/freework_scraping_dag.py
@@ -3,6 +3,10 @@ from datetime import datetime
 from freework_scripts import link_scraper, page_scraper
 from airflow.operators.bash import BashOperator
 from plugins.slack_webhook import task_success_slack_alert, task_failure_slack_alert
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+from airflow.providers.google.cloud.transfers.gcs_to_gcs import GCSToGCSOperator
+
+
 
 
  
@@ -31,6 +35,27 @@ def freework_job_scraper():
     def page_scraper_task():
         filter="cast(cast(insert_date as timestamp) as date) = current_date()"
         page_scraper.parse_job_details(condition=f"{filter}")
+
+    load_json_into_bq = GCSToBigQueryOperator(
+        task_id="load_jobs_to_bigquery",
+        bucket="freework_jobs",
+        source_objects=["freework/jobs/*.jsonl"],
+        source_format="NEWLINE_DELIMITED_JSON",
+        destination_project_dataset_table="dev-env-368414.freework.job_details",
+        write_disposition="WRITE_APPEND",
+        external_table=False,
+        autodetect=True,
+        deferrable=True,
+    )
+
+    move_loaded_file = GCSToGCSOperator(
+        task_id="move_loaded_file",
+        source_bucket="freework_jobs",
+        source_object="freework/jobs/*.jsonl",
+        destination_bucket="freework_jobs",
+        destination_object="freework/loaded/",
+        move_object=True,
+    )
     
     variable = f"{str(datetime.now().date())}"
     freelance_cleaning_task = BashOperator(task_id="freelance_cleaning_task",bash_command=f"cd /opt/airflow/dbt && \
@@ -80,7 +105,7 @@ def freework_job_scraper():
         on_success_callback=task_success_slack_alert)
 
 
-    get_page_number_task() >> link_scraper_task() >> page_scraper_task() >> [freelance_cleaning_task,cdi_cleaning_task,cdi_freelance_cleaning_task] >> jobs_cleaning_aggregation >> [jobs_modeling_experience,jobs_modeling_type,jobs_modeling_location,jobs_modeling_skills] >> jobs_modeling_fact_jobs_initial >> jobs_modeling_bridge_job_skill >> jobs_modeling_ref_job_category 
+    get_page_number_task() >> link_scraper_task() >> page_scraper_task() >> load_json_into_bq >> move_loaded_file >> [freelance_cleaning_task,cdi_cleaning_task,cdi_freelance_cleaning_task] >> jobs_cleaning_aggregation >> [jobs_modeling_experience,jobs_modeling_type,jobs_modeling_location,jobs_modeling_skills] >> jobs_modeling_fact_jobs_initial >> jobs_modeling_bridge_job_skill >> jobs_modeling_ref_job_category 
 
 freework_job_scraper()
 
